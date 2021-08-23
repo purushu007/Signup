47381f977799ec1182c37fa08b6f07f1bc12dd96
Purushu <purushu574@gmail.com>
2021-08-23 13:55:53 +0530

    Updated error messages in form

--- src/Components/SignUpForm/SignupForm.js
+++ src/Components/SignUpForm/SignupForm.js
@@ -94,24 +94,24 @@ export default class SignUpForm extends Component {
                             <div className="field-container">
                                 <input type="text" id="name" name="name" required aria-required="true" placeholder=" " className={this.state.errors && this.state.errors.name ? 'form-input error' : 'form-input name'} value={this.state.inputFields.name || ''} onChange={this.handleChange} noValidate aria-labelledby="Enter your name"/>
                                 <label htmlFor="name">Name</label>
-                                <div className="error-msg">Please enter your name</div>
+                                <div className="error-msg">{this.state.errors.name !== undefined ? this.state.errors.name : "Please enter your name"}</div>
                             </div>
                             <div className="field-container">
                                 <input type="email" id="email" name="email" required aria-required="true" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" placeholder=" " className={this.state.errors && this.state.errors.email ? 'form-input error' : 'form-input email'} value={this.state.inputFields.email || ''} onChange={this.handleChange} noValidate aria-labelledby="Enter your Email ID"/>
                                 <label htmlFor="email">Email</label>
-                                <div className="error-msg">Please enter valid email-ID.</div>
+                                <div className="error-msg">{this.state.errors.email !== undefined ? this.state.errors.email : "Please enter your email-ID."}</div>
                             </div>
                             <div className="field-container">
                                 <input type="password" id="password" name="password" required aria-required="true" minLength="6" placeholder=" " className={this.state.errors && this.state.errors.password ? 'form-input error' : 'form-input password'} value={this.state.inputFields.password || ''} onChange={this.handleChange} noValidate aria-labelledby="Enter Password"/>
                                 <label htmlFor="password">Password</label>
-                                <div className="error-msg">Password must be at least 6 characters long.</div>
+                                <div className="error-msg">{this.state.errors.password !== undefined ? this.state.errors.password : "Please enter a password"}</div>
                             </div>
                             <div className="terms-checkbox">
                                 <label htmlFor="subscribe" className="checkbox-label">
                                     <div className="terms-label-text">I agree to the<span>Terms</span>and<span>Privacy Policy</span></div>
                                     <input type="checkbox" name="checkbox" id="subscribe" className={this.state.errors && this.state.errors.checkbox ? 'form-input checkbox error' : 'form-input checkbox'} value={this.state.checkbox} checked={this.state.checkbox} onChange={this.handleCheckBox} noValidate/>
                                     <span className="check-mark"></span>
-                                    <div className="error-msg">Please check Terms & Policy</div>
+                                    <div className="error-msg">{this.state.errors.checkbox !== undefined ? this.state.errors.checkbox : "Please check Terms & Policy"}</div>
                                 </label>
                             </div>
                             <div className="submit-container">
--- src/Components/SignUpForm/SignupFormHooks.js
+++ src/Components/SignUpForm/SignupFormHooks.js
@@ -80,24 +80,24 @@ const SignUpFormHooks = () => {
                         <div className="field-container">
                             <input type="text" id="name" name="name" required aria-required="true" placeholder=" " className={errors && errors.name ? 'form-input error' : 'form-input name'} value={inputFields.name || ''} onChange={handleChange} noValidate aria-labelledby="Enter your name"/>
                             <label htmlFor="name">Name</label>
-                            <div className="error-msg">Please enter your name</div>
+                            <div className="error-msg">{errors.name !== undefined ? errors.name : "Please enter your name"}</div>
                         </div>
                         <div className="field-container">
                             <input type="email" id="email" name="email" required aria-required="true" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" placeholder=" " className={errors && errors.email ? 'form-input error' : 'form-input email'} value={inputFields.email || ''} onChange={handleChange} noValidate aria-labelledby="Enter your Email ID"/>
                             <label htmlFor="email">Email</label>
-                            <div className="error-msg">Please enter valid email-ID.</div>
+                            <div className="error-msg">{errors.email !== undefined ? errors.email : "Please enter your email-ID."}</div>
                         </div>
                         <div className="field-container">
                             <input type="password" id="password" name="password" required aria-required="true" minLength="6" placeholder=" " className={errors && errors.password ? 'form-input error' : 'form-input password'} value={inputFields.password || ''} onChange={handleChange} noValidate aria-labelledby="Enter Password"/>
                             <label htmlFor="password">Password</label>
-                            <div className="error-msg">Password must be at least 6 characters long.</div>
+                            <div className="error-msg">{errors.password !== undefined ? errors.password : "Please enter a password"}</div>
                         </div>
                         <div className="terms-checkbox">
                             <label htmlFor="subscribe" className="checkbox-label">
                                 <div className="terms-label-text">I agree to the<span>Terms</span>and<span>Privacy Policy</span></div>
                                 <input type="checkbox" name="checkbox" id="subscribe" className={errors && errors.checkbox ? 'form-input checkbox error' : 'form-input checkbox'} value={checkbox} checked={checkbox} onChange={handleCheckBox} noValidate/>
                                 <span className="check-mark"></span>
-                                <div className="error-msg">Please check Terms & Policy</div>
+                                <div className="error-msg">{errors.checkbox !== undefined ? errors.checkbox : "Please check Terms & Policy"}</div>
                             </label>
                         </div>
                         <div className="submit-container">
